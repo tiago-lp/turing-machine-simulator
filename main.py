d619d1dfba7d4ab3de31c0e7e8fbd4db2402c32c
Emanuel Joivo <emanueljoivo@gmail.com>
2017-08-19 03:07:52 -0300

    modularizando

--- main.py
+++ main.py
@@ -1,32 +1,48 @@
 # -*- coding: utf-8 -*-
 # Turing Machine Simulator
 
-from Tkinter import tkFileDialog
+from Tkinter import *
+
+root = Tk()
+
+def menu():
+	print("Choose an option:")
+	print("	[1] - Submit your own machine")
+	print("	[2] - Use some built-in machine")
+	print("	[3] - Exit")
+	print("")
+
+def examples_machines():
+	print("Load an example program:")
+	print("")
+	print("[1] - Binary to decimal conversion")
+	print("[2] - Palindrome detector")
+	print("")
+
+def invalid_option():
+	print("Something is wrong...")
+	print("... maybe you must try again choosing one of valid options")
+	print("")
 
 def main():
-	print("")
+	while True:
+		menu()
+		option = int(raw_input())
+
+		if (option == 1):
+			# open file and process to build a machine
+			print("option 1")
+
+		elif (option == 2):
+			examples_machines()	
+			example = int(raw_input())
+
+		elif (option == 3):
+			break
+	
+		else:
+			invalid_option()
+			
 
 if __name__ == "__main__":
 	main()
-
-while True:
-	print("Choose an option:")
-	print("[1] To submit your own machine")
-	print("[2] To use some built-in machine")
-	print("[3] Exit")
-
-	option = raw_input()
-
-	if (option == 1):
-		# open file and process to build a machine
-
-	elif (option == 2):
-		# open a list of the machines built-in
-		print("Choose one of these machines")
-
-	elif (option == 3):
-		break
-	
-	else:
-		print("Something is wrong...)
-		print("... maybe you must try choose one of valid options")
