e0deae529f227bf525276d6adc43fa6098e1e578
EmersonLucena <emersonleonardolucena@hotmail.com>
2017-08-29 23:56:57 -0300

    Turing Machine Itself
    File has some extra comments and one extra function(simula) - it is
    because front-end isn't ready yet.
    
    The function simula( ) basically fills the transition function manually
    and creates the tape, since it is not automatic yet.

---
+++ machine/turing_machine.py
@@ -0,0 +1,82 @@
+'''
+transition function delta: dictionary {(q0, r):(q1, w, D)}
+q0 -> old state
+q1 -> new state
+r  -> read from tape
+w  -> write on tape
+D  -> direction to move (L / D / *)
+'''
+
+delta = {}
+q_start = "0"
+tape = []
+
+#state needs to be a string
+def is_halt_state(state):
+    if len(state) < 4:
+        return False
+    else:
+        return state[0:4] == "halt" #starts with "halt"
+
+
+''' apenas para testar. PODE APAGAR DEPOIS
+ a intencao eh q dps do front-end estar codado, essa funcao seja apagada
+ e o delta{} e tape[] sejam preenchidos automaticamente '''
+def simula():
+    global tape
+    global delta
+
+    #tape precisa ser uma lista de caracteres
+    #por exemplo tape = ['a', 'a', 'Z', 'b', '_', '*']
+    tape = list("aaaaab")
+
+    #delta precisa assumir esse formato (descrito no inicio do codigo)
+    #delta[(estado, leitura)] = (novo_estado, escrita, direcao_movimento)
+    delta[("0", 'a')] = ("*", 'c', 'r')
+    delta[("0", 'b')] = ("voltando", 'd', 'l')
+    delta[("voltando", 'c')] = ("*", '*', 'l')
+    delta[("voltando", '_')] = ("halt-deubom", 'e', 'r')
+
+def run():
+    current_state = q_start
+    current_tape = tape
+    tape_position = 0
+
+    while not is_halt_state(current_state):
+        ## apos finalizar a saida no terminal, apagar este print
+        print "State:", current_state, "  Tape:", "".join(current_tape), "  Position:", tape_position
+
+        try:
+            if (current_state, current_tape[tape_position]) in delta:
+                (q1, w, D) = delta[(current_state, current_tape[tape_position])]
+            else:
+                (q1, w, D) = delta[(current_state, '*')] # * is the wildcard
+
+            if w != '*':
+                current_tape[tape_position] = w
+            if q1 != '*':
+                current_state = q1
+
+            if D == 'l':
+                if tape_position > 0:
+                    tape_position -= 1
+                else:
+                    current_tape.insert(0, '_') #increase tape at start, right shift
+            elif D == 'r':
+                tape_position += 1
+                if tape_position == len(current_tape): #increase tape at the end
+                    current_tape.append('_')
+
+        except KeyError:
+            # Transition doesnt exist!!
+            print "Error!"
+            break
+
+    ## apos finalizar a saida no terminal, apagar estes prints
+    print "Done"
+    print "Final State:", current_state
+    print "Final Tape:", "".join(current_tape)
+
+if __name__ == "__main__":
+    simula() ## PODE APAGAR DEPOIS
+    run()
